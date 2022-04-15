7b592b08da6bbd10102b38355aaf2b1bd3bd3e4c
Hayley <hwitherell@skylandanalytics.net>
2022-04-15 13:10:34 -0600

    Try different cors config code

--- config/application.rb
+++ config/application.rb
@@ -23,15 +23,11 @@ module TdlistApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # config.middleware.insert_before 0, "Rack::Cors" do
-    #   allow do
-    #     origins '*'
-    #     resource(
-    #       '*',
-    #       headers: :any,
-    #       methods: [:get, :patch, :put, :delete, :post, :options]
-    #       )
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
